2b663f22eb630afe012e6e5799c059b56c46ff27
davidwatkins73 <davidwatkins73@gmail.com>
2015-11-29 20:28:43 +0000

    Tracking

--- initial-code/SignalExamples.elm
+++ initial-code/SignalExamples.elm
@@ -5,10 +5,10 @@ import Keyboard
 import Time
 
 main =
-  Signal.map show Mouse.position
+--  Signal.map show Mouse.position
 --  Signal.map show (Time.every Time.second)
 --  Signal.map show Keyboard.arrows
---  Signal.map show Window.dimensions
+  Signal.map show Window.dimensions
 --  Signal.map show Keyboard.enter
 --  Signal.map show (Keyboard.isDown 32)
 
